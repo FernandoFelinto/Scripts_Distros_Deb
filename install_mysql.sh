7322a4530b01824b49b93a7a545aea96003fd5a7
FernandoFelinto <fernando.felinto@academico.ifpb.edu.br>
2024-01-07 01:30:46 -0300

    first commit

---
+++ install_mysql.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get install mysql-server
+
+sudo mysql
+
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'YOUR_PASSWORD';
+
+sudo mysql -u root -p
